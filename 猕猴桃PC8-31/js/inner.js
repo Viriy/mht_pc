553eb0d39e4db9c17fd1a83c69d9782001aaa4f1
Viriy <541239223@qq.com>
2018-09-04 11:13:44 +0800

    pc

---
+++ 猕猴桃PC8-31/js/inner.js
@@ -0,0 +1,28 @@
+﻿$(function() {
+    $(".row_nav a").hover(function(){
+        $(this).css("color","#fff").siblings().css("color","#666")
+    },function(){
+        $(this).css("color","#666");
+        $(".row_nav .active").css("color","#fff");
+    })
+    $(".row_nav").lavaLamp({
+        fx: "backout",
+        speed:70,
+        click: function(event, menuItem) {
+            $(this).addClass("active").siblings().removeClass("active")
+        }
+    });
+
+    //custominput();
+    $("input:radio").change(function() {
+        custominput();
+    })
+    $('input, textarea').placeholder();
+})
+
+function custominput() {
+    $("input:radio").siblings("i").removeClass("radio_checked");
+    $("input:radio:checked").siblings("i").addClass("radio_checked");
+    // $("input:checkbox").siblings(".icon").html("&#xe6dd;").removeClass("orange");
+    // $("input:checkbox:checked").siblings(".icon").html("&#xe6dc;").addClass("orange");
+}
